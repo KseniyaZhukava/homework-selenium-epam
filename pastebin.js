fb8163b4b0f2dd131829176e71f4e248e3566b3a
Kseniya Zhukava <kseniyazhukavaa@gmail.com>
2021-01-17 21:58:10 +0300

    first commit

---
+++ pastebin.js
@@ -0,0 +1,7 @@
+var webdriver = require('selenium-webdriver'),
+    By = webdriver.By,
+    until = webdriver.until;
+ 
+    var browser = new webdriver.Builder().usingServer().withCapabilities({'browserName': 'chrome' }).build();
+ 
+    browser.get('https://pastebin.com/');
