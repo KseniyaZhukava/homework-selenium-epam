ae84c77882a90b8c23de4f22ef54d7bc7bdb1c70
Kseniya Zhukava <kseniyazhukavaa@gmail.com>
2021-01-24 23:32:19 +0300

    First exsersise

--- pastebin.js
+++ pastebin.js
@@ -1,13 +1,13 @@
-var webdriver = require('selenium-webdriver'),
-    By = webdriver.By,
-    until = webdriver.until;
- 
-    var browser = new webdriver.Builder().usingServer().withCapabilities({'browserName': 'chrome' }).build();
- 
+let webdriver = require('selenium-webdriver'),
+    By = webdriver.By;
 
+let browser = new webdriver.Builder().usingServer().withCapabilities({ 'browserName': 'chrome' }).build();
 
-
-    browser.get('https://pastebin.com/');
-    var txtInput = browser.findElement(By.id('postform-text'));
-    txtInput.sendKeys("Hello from WebDriver");
-    
+browser.get('https://pastebin.com/');
+browser.findElement(By.id('postform-text')).sendKeys("Hello WebDriver");
+browser.findElement(By.id('postform-name')).sendKeys("Hello WebDriver");
+browser.findElement(By.id('select2-postform-expiration-container')).click().then(() => {
+   return browser.findElement(By.css('.select2-results__option:nth-child(3)')).click().then(
+       () =>  browser.findElement(By.xpath(`//button[text()='Create New Paste']`)).click()
+    );
+});
