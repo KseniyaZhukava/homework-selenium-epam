df94c1c4e3f6dfff6053a2da4ad5264895048331
KseniyaZhukava <kseniyazhukavaa@gmail.com>
2021-01-25 17:15:46 +0300

    first exercise

--- pastebin.js
+++ pastebin.js
@@ -4,8 +4,8 @@ let webdriver = require('selenium-webdriver'),
 let browser = new webdriver.Builder().usingServer().withCapabilities({ 'browserName': 'chrome' }).build();
 
 browser.get('https://pastebin.com/');
-browser.findElement(By.id('postform-text')).sendKeys("Hello WebDriver");
-browser.findElement(By.id('postform-name')).sendKeys("Hello WebDriver");
+browser.findElement(By.id('postform-text')).sendKeys("Hello from WebDriver");
+browser.findElement(By.id('postform-name')).sendKeys("helloweb");
 browser.findElement(By.id('select2-postform-expiration-container')).click().then(() => {
    return browser.findElement(By.css('.select2-results__option:nth-child(3)')).click().then(
        () =>  browser.findElement(By.xpath(`//button[text()='Create New Paste']`)).click()
