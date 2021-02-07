71690d178f33a8b8cc829221730e8b063078bd93
Kseniya Zhukava <kseniyazhukavaa@gmail.com>
2021-02-08 02:27:11 +0300

    2

--- test/BringItOn.js
+++ test/BringItOn.js
@@ -1,27 +1,24 @@
 const assert = require('chai').assert;
 const webdriver = require('selenium-webdriver'),
+    // { describe, it, after, before } = require('selenium-webdriver/testing'),
     By = webdriver.By,
     until = webdriver.until;
 
 let browser = new webdriver.Builder().usingServer().withCapabilities({ 'browserName': 'chrome' }).build();
     browser.get('https://pastebin.com/');
 
-NewPasteText = browser.findElement(By.id('postform-text')).sendKeys('git config --global user.name  "New Sheriff in Town"', webdriver.Key.ENTER, 'git reset $(git commit-tree HEAD^{tree} -m "Legacy code")', webdriver.Key.ENTER, 'git push origin master --force');
-TitleText = browser.findElement(By.id('postform-name')).sendKeys("how to gain dominance among developers");
-SyntaxHighlighting = browser.findElement(By.id('select2-postform-format-container'))
-SyntaxHighlightingBash = browser.findElement(By.xpath(".//li[@class='select2-results__option']/preceding::li[text()='Bash']"))
-PasteExpiration = browser.findElement(By.id('select2-postform-expiration-container'))
-PasteExpiration10M = browser.findElement(By.xpath("//ul[@id='select2-postform-expiration-results']/li[text()='10 Minutes']"))
-
+browser.findElement(By.id('postform-text')).sendKeys('git config --global user.name  "New Sheriff in Town"', webdriver.Key.ENTER, 'git reset $(git commit-tree HEAD^{tree} -m "Legacy code")', webdriver.Key.ENTER, 'git push origin master --force');
+browser.findElement(By.id('postform-name')).sendKeys("how to gain dominance among developers");
+    
     
 let selectSmt = async function () {
-    await SyntaxHighlighting.click();
-    await SyntaxHighlightingBash.click();    
+    await browser.findElement(By.id('select2-postform-format-container')).click();
+    await browser.findElement(By.xpath(".//li[@class='select2-results__option']/preceding::li[text()='Bash']")).click();    
 }
     
 let selectSmt2 = async function () {
-    await PasteExpiration.click();
-    await PasteExpiration10M.click();
+    await browser.findElement(By.id('select2-postform-expiration-container')).click();
+    await browser.findElement(By.xpath("//ul[@id='select2-postform-expiration-results']/li[text()='10 Minutes']")).click();
 }
     
 let selectSmt3 = async function () {
@@ -44,13 +41,8 @@ describe('open pastebin home page', function () {
         assert.equal(TabTitle, "how to gain dominance among developers", "Text of title is wrong");
     });
 
-    //     it('works with mocha', function(){
-    //         browser.findElement(By.id('select2-postform-expiration-container')).click();
-    //         browser.findElement(By.xpath("//ul[@id='select2-postform-expiration-results']/li[text()='10 Minutes']")).click();
-    //     });
+    it('works with mocha', function(){
 
-    //     it('works with mocha', function(){
-
-    //     });
+    });
 
     })
