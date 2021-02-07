153e4834ac3c091ab456d47cdc0e20b80fa0b37a
Kseniya Zhukava <kseniyazhukavaa@gmail.com>
2021-02-08 02:47:42 +0300

    2

--- test/BringItOn.js
+++ test/BringItOn.js
@@ -5,35 +5,43 @@ const webdriver = require('selenium-webdriver'),
     until = webdriver.until;
 
 let browser = new webdriver.Builder().usingServer().withCapabilities({ 'browserName': 'chrome' }).build();
-    browser.get('https://pastebin.com/');
-
-browser.findElement(By.id('postform-text')).sendKeys('git config --global user.name  "New Sheriff in Town"', webdriver.Key.ENTER, 'git reset $(git commit-tree HEAD^{tree} -m "Legacy code")', webdriver.Key.ENTER, 'git push origin master --force');
-browser.findElement(By.id('postform-name')).sendKeys("how to gain dominance among developers");
-    
-    
-let selectSmt = async function () {
-    await browser.findElement(By.id('select2-postform-format-container')).click();
-    await browser.findElement(By.xpath(".//li[@class='select2-results__option']/preceding::li[text()='Bash']")).click();    
-}
-    
-let selectSmt2 = async function () {
-    await browser.findElement(By.id('select2-postform-expiration-container')).click();
-    await browser.findElement(By.xpath("//ul[@id='select2-postform-expiration-results']/li[text()='10 Minutes']")).click();
-}
-    
-let selectSmt3 = async function () {
-    await selectSmt();
-    await selectSmt2();
-}
-    
-selectSmt3().then(
-    () => browser.findElement(By.xpath("//button[text()='Create New Paste']")).click()
-);
 
 describe('open pastebin home page', function () {
-    it('The syntax is suspended for bash', function(){
+
+    beforeEach(function(){
+        browser.get('https://pastebin.com/');
+
+        browser.findElement(By.id('postform-text')).sendKeys('git config --global user.name  "New Sheriff in Town"', webdriver.Key.ENTER, 'git reset $(git commit-tree HEAD^{tree} -m "Legacy code")', webdriver.Key.ENTER, 'git push origin master --force');
+        browser.findElement(By.id('postform-name')).sendKeys("how to gain dominance among developers");
+            
+        let selectSmt = async function () {
+            await browser.findElement(By.id('select2-postform-format-container')).click();
+            await browser.findElement(By.xpath(".//li[@class='select2-results__option']/preceding::li[text()='Bash']")).click();    
+        }
+            
+        let selectSmt2 = async function () {
+            await browser.findElement(By.id('select2-postform-expiration-container')).click();
+            await browser.findElement(By.xpath("//ul[@id='select2-postform-expiration-results']/li[text()='10 Minutes']")).click();
+        }
+            
+        let selectSmt3 = async function () {
+            await selectSmt();
+            await selectSmt2();
+        }
+            
+        selectSmt3().then(
+            () => browser.findElement(By.xpath("//button[text()='Create New Paste']")).click()
+        );
+
+    });
+
+    afterEach(function(){
+        browser.quit();
+    });
+
+    it('The syntax is suspended for Bash', function(){
         SyntaxHighlight = browser.findElement(By.xpath(".//div[@class='left']/a[@class='btn -small h_800']")).getText();
-        assert.equal(SyntaxHighlight, 'Bash');
+        assert.equal(SyntaxHighlight, "Bash", 'The syntax is NOT suspended for Bash');
     });
 
     it('Text of title contains Title', function () {
@@ -45,4 +53,4 @@ describe('open pastebin home page', function () {
 
     });
 
-    })
+});
